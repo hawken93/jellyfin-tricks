fdfd12205f10d887bcfe2ebba9a3551695001f15
hawken <hawken@thehawken.org>
2019-01-23 15:41:32 +0000

    Refining

--- rebase-crap.sh
+++ rebase-crap.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
 
-BRANCH=compile_warnings
-NEWBRANCH=compile_warnings_small
+BRANCH=ffprobe_verbosity
+NEWBRANCH=ffprobe_verbosity_small
 
 OLDDEST=origin/dev
 NEWDEST=dev_small
 
 set -e
 
+echo -e "\nFirst and foremost: Your escape sequence:"
+echo "  git checkout $BRANCH; git branch -D $NEWBRANCH"
+
+echo -e "\nFetching the newest commits from the interwebs"
 git fetch --all -p
 
+echo -e "\nChecking out $BRANCH"
 git checkout $BRANCH
 
-echo "Run a normal rebase first $BRANCH -> $OLDDEST"
-git rebase -i $OLDDEST
+echo -e "\nRun a normal rebase first $BRANCH -> $OLDDEST"
+git rebase $OLDDEST
 
-echo "Check out $NEWBRANCH"
+echo -e "\nCheck out $NEWBRANCH"
 git checkout -b $NEWBRANCH
 
-echo "Gather list of commits to preserve"
+echo -e "\nGather list of commits to preserve"
 git log --pretty=oneline $OLDDEST..$NEWBRANCH | sed 's/^\([^ ]*\) .*$/pick \1/' > /tmp/commit_list
 
 cat /tmp/commit_list
 
-echo "Rebase $NEWBRANCH -> $NEWDEST"
+echo -e "\nRebase $NEWBRANCH -> $NEWDEST"
 GIT_SEQUENCE_EDITOR="cp -v /tmp/commit_list" git rebase -i $NEWDEST
 
 rm -vf /tmp/commit_list
 
-echo "Things should be good now, my work is done here."
+echo -e "\nThings should be good now, my work is done here."
 echo "Read the following output and complete the rebase if one is in progress:"
 git status
