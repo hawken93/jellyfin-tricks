e612b1ccefae9e0930a90caf30443ef9f3926d64
hawken <hawken@thehawken.org>
2019-01-29 22:50:17 +0000

    make-ffmpeg.sh

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Just random scripts that can be useful with jellyfin development.
 
 
 ## rebase-crap.sh
+
 This script is intended to help rebase your branch after jellyfin is minified. Make sure to edit the varibles
 in the script before you run it, and that you also read and understand it. I can't make any guarantees, so take backups if you are
 unsure.
@@ -21,3 +22,8 @@ NEWDEST is upstream/master.
 OLDDEST is probably dev or origin/dev (the old dev branch).
 
 OLDDEST is origin/dev since you probably didn't delete it yet. In an emergency, I have an olddev branch in [my fork](https://github.com/hawken93/jellyfin/tree/olddev) :)
+
+## make-ffmpeg.sh
+This script tries to install ffmpeg 4.0.3 with nvenc/nvdec/vaapi in /usr/local/bin/ffmpeg. It will clone some repos to /usr/src and do some magic.
+
+Should be relatively safe to run but it might install a ton of dependencies without asking you. Remove the -v in apt-get install if this is concerning :P
---
+++ make-ffmpeg.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+apt-get install -y \
+	yasm libchromaprint-dev libgnutls28-dev liblilv-dev libass-dev libbluray-dev libbs2b-dev \
+	libcaca-dev libcodec2-dev libdrm-dev flite1-dev libgme-dev libgsm1-dev libmp3lame-dev \
+	libx264-dev libx265-dev libopenjp2-7-dev libopenmpt-dev libopus-dev \
+	librsvg2-dev librubberband-dev libshine-dev libsnappy-dev libsoxr-dev libssh-dev libspeex-dev \
+	libtheora-dev libtwolame-dev libvpx-dev libwavpack-dev libwebp-dev libxvidcore-dev libzmq3-dev \
+	libzvbi-dev libxml2-dev libcdio-dev libcdio-paranoia-dev libva-dev
+
+cd /usr/src
+if [ ! -d ffmpeg ]; then
+	git clone git://source.ffmpeg.org/ffmpeg.git
+fi
+if [ ! -d nv-codec-headers ]; then
+	git clone git://source.ffmpeg.org/ffmpeg/nv-codec-headers.git
+fi
+
+cd /usr/src/nv-codec-headers
+git pull --rebase
+make install
+
+cd /usr/src/ffmpeg
+#git fetch --all -p
+#git checkout n4.0.3
+
+./configure \
+	--prefix=/usr/local \
+	--enable-gpl \
+	--enable-avisynth --enable-gnutls --enable-libass --enable-libbluray --enable-libbs2b \
+	--enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libflite --enable-libfontconfig \
+	--enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm \
+	--enable-libmp3lame --enable-libopenjpeg --enable-libopenmpt --enable-libopus \
+	--enable-librsvg --enable-librubberband --enable-libshine --enable-libsnappy \
+	--enable-libsoxr --enable-libspeex --enable-libssh --enable-libtheora --enable-libtwolame \
+	--enable-libvorbis --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx265 \
+	--enable-libxml2 --enable-libxvid --enable-libzmq --enable-libzvbi --enable-lv2 \
+	--enable-libdrm \
+	--enable-chromaprint --enable-libx264 \
+	--enable-nvenc --enable-nvdec --enable-vaapi
+
+make -j$(nproc)
+make install
